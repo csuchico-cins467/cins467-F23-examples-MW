54008f2f35b5c67c3b635b6f2865a882dcddfe17
Bryan Dixon <javawolfpack@gmail.com>
2023-10-09 16:54:05 -0700

    Google Auth

--- form_example/lib/main.dart
+++ form_example/lib/main.dart
@@ -3,11 +3,22 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:form_example/login.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+GoogleSignIn googleSignIn = GoogleSignIn(
+  scopes: [
+    'email',
+  ],
+);
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    title: "Login Page",
+    home: LoginPage(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
